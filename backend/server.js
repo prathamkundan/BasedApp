cb643e768a437243d1e5fc3a87e3990cd7d1461e
prathamkundan <2020csb1114@iitrpr.ac.in>
2022-07-03 20:25:44 +0530

    preparing deployment

--- backend/server.js
+++ backend/server.js
@@ -1,3 +1,4 @@
+const path = require('path')
 const express = require("express");
 const dotenv = require("dotenv").config();
 const app = express();
@@ -5,10 +6,6 @@ const { errorHandler } = require('./Middleware/error.middleware');
 const { connectDB } = require('./config/db');
 
 const PORT = process.env.PORT || 5000;
-
-app.get('/', (req, res) => {
-    res.send("Hello World!");
-})
 
 // connecting to db
 connectDB();
@@ -20,6 +17,16 @@ app.use(express.urlencoded({ extended: false }));
 // Routes
 app.use("/api/posts", require("./Routes/posts.routes"));
 app.use("/api/users", require("./Routes/user.routes"));
+
+if (process.env.NODE_ENV === 'production') {
+    app.use(express.static(path.join(__dirname, '../frontend/build')));
+
+    app.get('*', (req, res) =>
+        res.sendFile(
+            path.resolve(__dirname, '../', 'frontend', 'build', 'index.html')
+        )
+    )
+}
 
 // Custom error handler
 app.use(errorHandler);
